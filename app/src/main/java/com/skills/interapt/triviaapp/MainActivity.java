f209e9cf490293d99bc4b8d149aafc6d730bc07a
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-19 14:16:38 -0400

    Developed QuestionCreatorFragment,java

--- app/src/main/java/com/skills/interapt/triviaapp/MainActivity.java
+++ app/src/main/java/com/skills/interapt/triviaapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.skills.interapt.triviaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.add_question_button)
+    protected void addQuestionClicked(){
+        questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+    }
+
+    @OnClick(R.id.take_quiz_button)
+    protected void takeQuiz(){
+    }
+
+    @OnClick(R.id.delete_quiz_button)
+    protected void deleteQuiz(){
     }
 }
