80cccd62dcc3229e071cb24645f9e59d6bb1546f
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-20 10:35:35 -0400

    Bound the Buttons in the TakeQuiz fragment

--- app/src/main/java/com/skills/interapt/triviaapp/TakeQuizFragment.java
+++ app/src/main/java/com/skills/interapt/triviaapp/TakeQuizFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,8 +16,25 @@ import butterknife.OnClick;
 
 public class TakeQuizFragment extends Fragment {
 
+
     @BindView(R.id.question_take_quiz_textview)
     protected TextView question_take_quiz_textview;
+
+    @BindView(R.id.first_answer_button)
+    protected Button first_answer_button;
+
+    @BindView(R.id.second_answer_button)
+    protected Button second_answer_button;
+
+    @BindView(R.id.third_answer_button)
+    protected Button third_answer_button;
+
+    @BindView(R.id.fourth_answer_button)
+    protected Button fourth_answer_button;
+
+
+
+
 
     @Nullable
     @Override
@@ -28,7 +46,6 @@ public class TakeQuizFragment extends Fragment {
         return view;
     }
 
-    @OnClick(R.id.first_answer_button)
 
     public static TakeQuizFragment newInstance() {
 
@@ -37,6 +54,36 @@ public class TakeQuizFragment extends Fragment {
         TakeQuizFragment fragment = new TakeQuizFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+
+
+
+
+    @OnClick(R.id.first_answer_button)
+    protected void buttonOneClicked(){
+
+    }
+
+
+    @OnClick(R.id.second_answer_button)
+    protected void buttonTwoClicked(){
+
+    }
+
+    @OnClick(R.id.third_answer_button)
+    protected void buttonThreeClicked(){
+
+    }
+
+    @OnClick(R.id.fourth_answer_button)
+    protected void buttonFourClicked(){
+
+    }
+
+    @OnClick(R.id.next_button)
+    protected void buttonNextClicked(){
+
     }
 
 
