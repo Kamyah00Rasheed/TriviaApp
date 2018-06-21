215fbdaed4eff5e5657633a87bcd200e64c469ac
Kamyah Rasheed <kamyah.rasheed@stu.jefferson.kyschools.us>
2018-06-21 15:25:05 -0400

    Finished Project

--- app/src/main/java/com/skills/interapt/triviaapp/MainActivity.java
+++ app/src/main/java/com/skills/interapt/triviaapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.skills.interapt.triviaapp;
 
+import android.content.DialogInterface;
 import android.os.Parcelable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -13,7 +13,7 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements QuestionCreatorFragment.Callback{
+public class MainActivity extends AppCompatActivity implements QuestionCreatorFragment.Callback, QuizFragment.QuizCallback{
 
     private QuestionCreatorFragment questionCreatorFragment;
     private QuizFragment quizFragment;
@@ -35,13 +35,6 @@ public class MainActivity extends AppCompatActivity implements QuestionCreatorFr
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
     }
 
-    @OnClick(R.id.take_quiz_button)
-    protected void takeQuiz(){
-    }
-
-    @OnClick(R.id.delete_quiz_button)
-    protected void deleteQuiz(){
-    }
 
     @Override
     public void questionSaved(Question question) {
@@ -65,9 +58,60 @@ public class MainActivity extends AppCompatActivity implements QuestionCreatorFr
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, quizFragment).commit();
 
             Bundle bundle = new Bundle();
+            quizFragment.attachParent(this);
             bundle.putParcelableArrayList(QUESTIONS_LIST, (ArrayList<? extends Parcelable>) questionsList);
+            quizFragment.setArguments(bundle);
 
         }
     }
 
+    @Override
+    public void quizFinished(int correctAnswers) {
+        //Removes the fragment from the frameLayout
+        getSupportFragmentManager().beginTransaction().remove(quizFragment).commit();
+
+        showQuizResultsAlertDialog(correctAnswers);
+    }
+
+    private void showQuizResultsAlertDialog(int correctAnswers) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quiz Finished!")
+                .setMessage(getString(R.string.number_of_correct_answers, correctAnswers))
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
+    }
+
+    @OnClick(R.id.delete_quiz_button)
+    protected void deleteQuizClicked() {
+
+        if(questionsList.isEmpty()) {
+            Toast.makeText(this, "There is no quiz to delete! :(", Toast.LENGTH_SHORT).show();
+
+        } else {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete Quiz")
+                .setMessage("Are you sure you want to delete this quiz?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Handle deleting quiz
+                        questionsList.clear();
+                        dialogInterface.dismiss();
+                        Toast.makeText(MainActivity.this, "Quiz Deleted!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Handle closing dialog
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }}
 }
--- app/src/main/java/com/skills/interapt/triviaapp/QuizFragment.java
+++ app/src/main/java/com/skills/interapt/triviaapp/QuizFragment.java
@@ -22,8 +22,8 @@ import static com.skills.interapt.triviaapp.MainActivity.QUESTIONS_LIST;
 public class QuizFragment extends Fragment {
 
 
-    @BindView(R.id.question_take_quiz_textview)
-    protected TextView question_take_quiz_textview;
+    @BindView(R.id.quiz_question_textview)
+    protected TextView quizQuestion;
 
     @BindView(R.id.first_answer_button)
     protected Button firstAnswerButton;
@@ -40,13 +40,15 @@ public class QuizFragment extends Fragment {
     private List<Question> questionsList;
     private Question question;
     private int questionListPosition =0;
+    private int correctAnswers = 0;
+    private QuizCallback quizCallback;
 
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_take_quiz, container, false); //always choose false for this
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false); //always choose false for this
 
         ButterKnife.bind(this, view);
 
@@ -75,7 +77,7 @@ public class QuizFragment extends Fragment {
 
     private void populateQuizContent() {
         question = questionsList.get(questionListPosition);
-        question_take_quiz_textview.setText(question.getQuestion());
+        quizQuestion.setText(question.getQuestion());
 
         List<Button> buttonList = new ArrayList<>();
         buttonList.add(firstAnswerButton);
@@ -83,21 +85,49 @@ public class QuizFragment extends Fragment {
         buttonList.add(thirdAnswerButton);
         buttonList.add(fourthAnswerButton);
 
+        //Just like with the buttons, this arrayList will take all of the possible answers
+        //and allows us to access them
         List<String> possibleAnswersList = new ArrayList<>();
         possibleAnswersList.add(question.getCorrectAnswer());
         possibleAnswersList.add(question.getWrongAnswerOne());
         possibleAnswersList.add(question.getWrongAnswerTwo());
         possibleAnswersList.add(question.getWrongAnswerThree());
 
+        //This For Each loop takes the arrayLists we made and actually allow us to randomize
+        //what question goes on which button
         for (Button button : buttonList){
+            //Creates a random number that will allow us to pick an answer from our arrayList
             int random = (int) Math.ceil(Math.random() * (possibleAnswersList.size() -1));
+            //Using the random number above we will set the text of the button by getting that item
+            //from the possible answers list
             button.setText(possibleAnswersList.get(random));
+            //To make sure we don't use the same answer twice we remove the possible answer from
+            //the list
             possibleAnswersList.remove(random);
         }
     }
 
+    private void checkAnswer(String answer){
+        disableAnswerButtons();
+        //Increments questionListPosition so we can go to the next question
+        questionListPosition++;
+
+        if(question.getCorrectAnswer().equals(answer)) {
+            //Sets the textView to show the user they were correct
+            quizQuestion.setText(R.string.correct);
+            //Increments the correct answers the user has gotten
+            correctAnswers++;
+        } else {
+            quizQuestion.setText(getString(R.string.wrong_answer_text, question.getCorrectAnswer()));
+
+        }
+
+    }
+
     @OnClick(R.id.first_answer_button)
     protected void buttonOneClicked(){
+
+        checkAnswer(firstAnswerButton.getText().toString());
 
     }
 
@@ -105,21 +135,62 @@ public class QuizFragment extends Fragment {
     @OnClick(R.id.second_answer_button)
     protected void buttonTwoClicked(){
 
+        checkAnswer(secondAnswerButton.getText().toString());
+
     }
 
     @OnClick(R.id.third_answer_button)
     protected void buttonThreeClicked(){
+
+        checkAnswer(thirdAnswerButton.getText().toString());
 
     }
 
     @OnClick(R.id.fourth_answer_button)
     protected void buttonFourClicked(){
 
+        checkAnswer(fourthAnswerButton.getText().toString());
+
     }
 
     @OnClick(R.id.next_button)
     protected void buttonNextClicked(){
+        enableAnswerButtons();
+        if(questionListPosition <= questionsList.size() -1) {
+            populateQuizContent();
+        } else {
+            //Handling no more questions, taking user back to MainActivity
+            quizCallback.quizFinished(correctAnswers);
 
+        }
+
+    }
+
+    private void disableAnswerButtons(){
+        firstAnswerButton.setEnabled(false);
+        secondAnswerButton.setEnabled(false);
+        thirdAnswerButton.setEnabled(false);
+        fourthAnswerButton.setEnabled(false);
+
+    }
+
+    private void enableAnswerButtons(){
+        firstAnswerButton.setEnabled(true);
+        secondAnswerButton.setEnabled(true);
+        thirdAnswerButton.setEnabled(true);
+        fourthAnswerButton.setEnabled(true);
+
+
+    }
+
+    public void attachParent(QuizCallback quizCallback){
+
+        this.quizCallback = quizCallback;
+
+    }
+
+    public interface QuizCallback {
+        void quizFinished(int correctAnswers);
     }
 
 
